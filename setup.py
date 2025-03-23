c21d80520d3ed412869c09fa583a243671cf11fe
csh4bj <csh4bj@virginia.edu>
2025-03-23 17:12:50 -0400

    This is my M09 HW

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='booklover',
+    version='0.1',
+    description='A package for HW09',
+    url='https://github.com/csh4bj/M09-HW',
+    author='Cameron Sukgoo Her',
+    author_email='csh4bj@virginia.edu',
+    license='MIT',
+    packages=['bookloverx']
+)
+
